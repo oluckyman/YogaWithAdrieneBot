99852e1bfb8346b85de135b07ce2c36afc9f5a39
ilya <ilya@belsky.in>
2020-04-15 14:55:33 +0200

    Show greetings

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const _ = require('lodash')
 const dotenv = require('dotenv')
 const Telegraf = require('telegraf')
 
@@ -10,18 +11,43 @@ bot.use(async (ctx, next) => {
   await next()
   const ms = new Date() - start
   console.log('---------')
-  console.log(ctx.update)
+  if (_.get(ctx.update, 'message.from.username') !== 'oluckyman') {
+    console.info(ctx.update)
+  } else { console.log('just me') }
   console.log('Response time: %sms', ms)
 })
 
-bot.on('text', (ctx) => ctx.reply(
-'Coming soon… Really soon, in a week or so.\nMeantime pick one from the April\'s playlist: https://www.youtube.com/playlist?list=PLui6Eyny-Uzy9eeFCGMO7dJNX4TZuQ1VM'
-))
+const greetings = [
+  [0.0, '⏳ _Coming soon…_'],
+  [2.2, '⌛ _Really soon, in a week or so._'],
+  [3.5, 'Meantime, pick some video from the [April playlist](https://www.youtube.com/playlist?list=PLui6Eyny-Uzy9eeFCGMO7dJNX4TZuQ1VM).'],
+]
+
+bot.command('/start', ctx => {
+  const sendGreeting = i => {
+    if (i >= greetings.length) return
+    const message = greetings[i][1]
+    const delaySec = _.get(greetings, [i + 1, 0])
+    ctx.replyWithMarkdown(message).then(() => {
+      if (delaySec !== undefined) {
+        setTimeout(() => sendGreeting(i + 1), delaySec * 1000)
+      }
+    })
+  }
+  sendGreeting(0)
+});
+
+
+bot.on('text', (ctx) => ctx.reply('… still working, give me some time👨‍💻'))
 
 
 const { PORT = 5000, HOST, WEBHOOK_SECRET } = process.env
 
-bot.telegram.setWebhook(`https://${HOST}/${WEBHOOK_SECRET}`)
-bot.startWebhook(`/${WEBHOOK_SECRET}`, null, PORT)
+bot.launch({
+  webhook: {
+    domain: `https://${HOST}/${WEBHOOK_SECRET}`,
+    port: PORT
+  }
+})
 
 
