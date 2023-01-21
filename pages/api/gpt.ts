3e1a12f4a68cb6a6adeca6387cffe71f67436017
Ilya Belsky <elias.logins@gmail.com>
2023-01-21 11:42:56 +0100

    Get completion from OpenAI (direct, without prompt instructions)

--- pages/api/gpt.ts
+++ pages/api/gpt.ts
@@ -1,17 +1,35 @@
+import { Configuration, OpenAIApi } from 'openai'
 import { RouterBuilder } from 'next-api-handler'
-import { setTimeout } from 'node:timers/promises'
+import * as config from '@/config'
 
-const router = new RouterBuilder()
+const openai = new OpenAIApi(
+  new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  })
+)
 
+const router = new RouterBuilder({ showMessage: true })
 router.get<{ answer: string }>(async (req) => {
   const query = req.query.query as string
   const answer = await askGpt(query)
   return { answer }
 })
+export default router.build()
 
 export async function askGpt(query: string) {
-  await setTimeout(1000)
-  return query + new Date()
+  const prompt = query // buildPrompt(query)
+  const answer = await sendPrompt(prompt)
+  return answer
 }
 
-export default router.build()
+async function sendPrompt(prompt: string, max_tokens = 256, temperature = 0.4) {
+  console.info('===> sendPrompt', { prompt, max_tokens, temperature })
+  const response = await openai.createCompletion({
+    model: config.openaiCompletionModel,
+    prompt,
+    max_tokens,
+    temperature,
+  })
+  console.info('===> got prompt', response.data)
+  return response.data.choices[0].text.trim()
+}
---
+++ src/config.ts
@@ -0,0 +1,2 @@
+// eslint-disable-next-line import/prefer-default-export
+export const openaiCompletionModel = 'text-davinci-003'
